60448e45cc0051e49cf95f41edc7024465d24717
TilStack <israel01tientcheu@gmail.com>
2025-06-30 20:26:25 +0100

    New file for the interconnexion between static and basique pages.

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const app = express()
 
 const {join} = require("path") // Pour la gestion des fichiers et des chemins
 
+app.use(express.static("public")) // Pour servir les fichiers statiques
+
 app.get("/test",function (req,res) {
     res.send("Hello, World!")  
 })
 
 app.get("/", function (req, res) {
-    res.sendFile(join())
+    res.sendFile(join(__dirname, '/views/index.html'))
 })
+
+app.get("/second", function (req, res) {
+    res.sendFile(join(__dirname, '/views/secondpage.html'))
+})
+
 
 app.listen(3000, function () {
     console.log("Server listening on http://localhost:3000")
