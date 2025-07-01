8aa61af73baf353cb59686c458f2bb71b6ee55ad
TilStack <israel01tientcheu@gmail.com>
2025-07-01 02:33:10 +0100

    some test

--- index.js
+++ index.js
@@ -18,6 +18,41 @@ app.get("/second", function (req, res) {
     res.sendFile(join(__dirname, '/views/secondpage.html'))
 })
 
+// Test de la page seconde avec des paramètres de requête
+app.get("/seconde", function (req, res) {
+    console.log("Second page accessed")
+
+    console.log(req.query) // Log the query parameters to the console
+   
+    const {x, y , operation } = req.query;
+
+    if(x && y && operation) {
+        let result;
+        switch(operation) {
+            case 'add':
+                result = parseFloat(x) + parseFloat(y);
+                break;
+            case 'sous':
+                result = parseFloat(x) - parseFloat(y);
+                break;
+            case 'multi':
+                result = parseFloat(x) * parseFloat(y);
+                break;
+            case 'div':
+                if (parseFloat(y) !== 0) {
+                    result = parseFloat(x) / parseFloat(y);
+                } else {
+                    return res.send('Error: Division by zero');
+                }
+                break;
+            default:
+                return res.status(400).send('Invalid operation');
+        }
+        res.send(`Result: ${result}`);
+    }
+    res.send('This is the second page')
+})
+
 
 app.listen(3000, function () {
     console.log("Server listening on http://localhost:3000")
